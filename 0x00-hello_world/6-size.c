a54155f05210c83b7955b76276e796dd6011618d
Osama-Hesham <osamahesham982@gmail.com>
2023-03-16 16:51:08 -0700

    size of variables

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,9 +6,9 @@
 int main(void)
 {
 
-	printf(%d\n,sizeof(int));
-	printf(%d\n,sizeof(short));
-	printf(%d\n,sizeof(long));
-	printf(%d\n,sizeof(char));
+	printf("%d\n",sizeof(int));
+	printf("%d\n",sizeof(short));
+	printf("%d\n",sizeof(long));
+	printf("%d\n",sizeof(char));
 	return (0);
 }
