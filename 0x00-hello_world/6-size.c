ea03cc6230b4fa51cea960303d96b4cae93d1e59
Osama-Hesham <osamahesham982@gmail.com>
2023-03-16 16:49:43 -0700

    size of vavriables

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - prints size of variables
+ * Return - 0 (success)
+ */
+int main(void)
+{
+
+	printf(%d\n,sizeof(int));
+	printf(%d\n,sizeof(short));
+	printf(%d\n,sizeof(long));
+	printf(%d\n,sizeof(char));
+	return (0);
+}
