66ce1edad2ef2817bbba6da063929025cf169212
Osama-Hesham <osamahesham982@gmail.com>
2023-03-16 17:08:55 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - prints a line
+ * Return: 1 (success)
+ */
+int main(void)
+{
+
+	cprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return(1);
+}
