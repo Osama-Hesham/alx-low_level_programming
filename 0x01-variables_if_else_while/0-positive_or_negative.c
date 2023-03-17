f1106c81e76003b375d2bbbfe99cadb6b86123d9
Osama-Hesham <osamahesham982@gmail.com>
2023-03-17 04:03:45 -0700

    if, else if, else

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include <time.h>
+/**
+ * main - check if variable is +ve or -ve
+ * Return: variable
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+	if(n>0)
+		printf("is positive");
+	else if(n==0)
+		printf("is zero");
+	else
+		printf("is negative");
+	
+	return (0);
+}
