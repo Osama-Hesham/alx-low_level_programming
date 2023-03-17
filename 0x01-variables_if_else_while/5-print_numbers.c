d9237a465b98c6740598a6930d9d31100cd72d34
Osama-Hesham <osamahesham982@gmail.com>
2023-03-17 15:31:07 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print blablabla
+ * Return: blablabla
+ */
+int main(void)
+{
+	int i = 0;
+
+	while(i != 0)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
