92e4496c4f2dc5ed0c3c0e6a107e6178682c650a
Osama-Hesham <osamahesham982@gmail.com>
2023-03-21 16:11:16 -0700

    alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - blablabla
+ * Return: blablalba
+ */
+void print_alphabet_x10(void)
+{ 
+	char c;
+	int i;
+	
+	for (i = 0; i <	10; i++)
+	{
+		for (c = 'a'; i <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
