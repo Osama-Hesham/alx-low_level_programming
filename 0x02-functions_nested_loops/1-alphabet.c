332ab78c0bbd5aa9fab3236e93f21eaa6d844a01
Osama-Hesham <osamahesham982@gmail.com>
2023-03-21 15:59:18 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main- blablabla
+ * Return: blablalba
+ */
+int main(void)
+{       
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	putchar('\n');
+
+}
