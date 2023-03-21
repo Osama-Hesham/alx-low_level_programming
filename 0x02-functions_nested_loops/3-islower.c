dc68427b40fc69c98295c6b3faee215bd721775b
Osama-Hesham <osamahesham982@gmail.com>
2023-03-21 16:20:03 -0700

    lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - blalbla
+ * Return: 0 or 1
+ */
+int _islower(int c)
+{
+	char c;
+
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,12 +1,30 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * main - blablabla
  *
- * Return: Always 0.
+ * Return: 0
  */
 int main(void)
 {
-    print_alphabet();
+    int r;
+		
+
+		
+    r = _islower('H');
+		
+    _putchar(r + '0');
+		
+    r = _islower('o');
+		
+    _putchar(r + '0');
+		
+    r = _islower(108);
+		
+    _putchar(r + '0');
+		
+    _putchar('\n');
+		
     return (0);
+
 }
