5acc72d420866e40b8adf9bdd7eabdbd4a116971
Osama-Hesham <osamahesham982@gmail.com>
2023-03-23 13:20:26 -0700

    upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isupper - blalbalba
+ * @c: char 
+ * Return: 0 or 1
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
