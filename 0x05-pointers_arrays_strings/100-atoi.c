44ea1f3bf54959a499d7dbb3670be00ed79bbb5d
Osama-Hesham <osamahesham982@gmail.com>
2023-03-27 07:28:20 -0700

    test

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,14 +11,14 @@ int _atoi(char *s)
 	int x;
 	int c = 0;
 	int i = 0;
-	int len = 0;
 	int number = 0;
 
 	while (*s != '\0')
 	{
 		if (s[i] >= '0' && s[i] <= '9')
 		{
-			number = number * 10 + atoi(s[i]);
+			x = atoi(s[i]);
+			number = number * 10 + x;
 		}
 		s++;
 		c++;
