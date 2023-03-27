62faf83eb1e4da95a7ebdc296229ec0e8ebd9838
Osama-Hesham <osamahesham982@gmail.com>
2023-03-27 03:19:38 -0700

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - prints a string, followed by a new line, to stdout
+ * @str: string to print
+ */
+void print_rev(char *str)
+{
+	int i = 0;
+
+	while (*str != '\0')
+	{
+		i++;
+		str++
+	}
+	while(i != -1)
+	{
+		_putchar(*(str+i));
+		i--;
+	}
+	_putchar('\n');
+}
