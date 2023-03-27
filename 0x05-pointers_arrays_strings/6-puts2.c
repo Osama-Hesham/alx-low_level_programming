c58fd6c3045704d0d57b5caeec467c4d839b0f56
Osama-Hesham <osamahesham982@gmail.com>
2023-03-27 04:07:30 -0700

    2 chars

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * puts2 - print every 2 chars
+ * @str: string
+ * Return: every 2 chars
+ */
+void puts2(char *str)
+{
+	int c = 0;
+	int i;
+	int *ptr = str;
+
+	while (*str != '\0')
+	{
+		c++;
+		str++;
+	}
+
+	for (i = 0; i < c; i+2)
+	{
+		if (*(ptr + i) != '\0')
+		{
+		_putchar(*(ptr + i));
+		}
+	}
+	_putchar('\n');
+}
