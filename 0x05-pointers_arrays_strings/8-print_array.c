db693676156956a22c80e287c4ff4e217496290f
Osama-Hesham <osamahesham982@gmail.com>
2023-03-27 06:25:48 -0700

    array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -7,16 +7,15 @@
  */
 void print_array(int *a, int n)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n - 1; i++)
 	{
-		if(i == n - 1)
-		{
-			printf("%d", a[i]);
-			break;
-		}
 		printf("%d, ", a[i]);
 	}
-	_putchar('\n');
+	if (i == (n - 1))
+	{
+		printf("%d", a[i]);
+	}
+	printf("\n");
 }
