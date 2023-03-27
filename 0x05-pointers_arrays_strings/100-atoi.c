7e6fff51b4889cf3a390250ad4035d831cd11526
Osama-Hesham <osamahesham982@gmail.com>
2023-03-27 06:58:35 -0700

    test

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ * Return: the int converted from the string
+ */
+int _atoi(char *s)
+{
+	int x = atoi(s);
+
+	printf("%d\n", x);
+}
