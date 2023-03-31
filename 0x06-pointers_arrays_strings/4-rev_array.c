f8d638ba98b517496bdd3a1dc9e85093b337c645
Osama-Hesham <osamahesham982@gmail.com>
2023-03-30 17:06:05 -0700

    reverse arr

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * reverse_array - reverse arr
+ * @a: input
+ * @n: input
+ * Return: output
+ */
+void reverse_array(int *a, int n)
+{
+	int i,temp;
+
+	for (i = 0; i < n--; i++)
+	{
+		temp = a[i];
+		a[i] = a[n];
+		a[n] = temp;
+	}
+}
