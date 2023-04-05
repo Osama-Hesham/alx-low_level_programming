c46fd82c2c3a0d60a3feed84e79726e6ae303ae9
Osama-Hesham <osamahesham982@gmail.com>
2023-04-05 01:15:07 -0700

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,15 +7,19 @@
  */
 int sqrt_helper(int n, int guess)
 {
-    if (guess * guess == n)
-        return (guess);
-
-    if (guess * guess > n)
-        return (-1);
-
-    return (sqrt_helper(n, guess + 1));
+	if (guess * guess == n)
+	{
+		return (guess);
+	}
+	else if (guess * guess > n)
+	{	
+		return (-1);
+	}
+	else
+	{
+		return (sqrt_helper(n, guess + 1));
+	}
 }
-
 /**
  * _sqrt_recursion - sqrt
  * @n: input 
@@ -23,8 +27,12 @@ int sqrt_helper(int n, int guess)
  */
 int _sqrt_recursion(int n)
 {
-    if (n < 0)
-        return (-1);
-
-    return (sqrt_helper(n, 0));
+	if (n < 0)
+	{
+        	return (-1);
+	}
+	else
+	{
+		return (sqrt_helper(n, 0));
+	}
 }
