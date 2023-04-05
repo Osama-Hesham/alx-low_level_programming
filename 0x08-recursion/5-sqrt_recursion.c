46c77ef98badb2761bf82fbec06e3aea6f640ef9
Osama-Hesham <osamahesham982@gmail.com>
2023-04-05 01:11:57 -0700

    sqrt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * sqrt_helper - sqrt helper
+ * @n: input1
+ * @guess: input2
+ * Return: output
+ */
+int sqrt_helper(int n, int guess)
+{
+    if (guess * guess == n)
+        return (guess);
+
+    if (guess * guess > n)
+        return (-1);
+
+    return (sqrt_helper(n, guess + 1));
+}
+
+/**
+ * _sqrt_recursion - sqrt
+ * @n: input 
+ * Return: output
+ */
+int _sqrt_recursion(int n)
+{
+    if (n < 0)
+        return (-1);
+
+    return (sqrt_helper(n, 0));
+}
