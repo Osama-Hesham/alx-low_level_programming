e0b83c70e438cb2fea9996ca3a1fccafc4b6784a
Osama-Hesham <osamahesham982@gmail.com>
2023-04-11 05:57:36 -0700

    concat arg

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * argstostr - concat all args
+ * @ac: input
+ * @av: input
+ * Return: output
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j;
+	char *str = malloc(sizeof(char) * ac * strlen(av) + 1);
+	int cnt1 = 0;
+	int cnt2 = 0'
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; j < av[i][j]; j++)
+		{
+			cnt1++;
+		}
+	}
+
+	cnt1 += ac;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; j < av[i][j]; j++)
+		{
+			str[cnt2] = av[i][j];
+			cnt2++;
+		}
+		if (str[cnt2] == '\0')
+		{
+			str[cnt2] = '\n';
+			cnt2++;
+		}
+	}
+	return (str);
+}
