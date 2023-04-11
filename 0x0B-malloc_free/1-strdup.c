f710408dd460630bf0671baab890f237cb36393c
Osama-Hesham <osamahesham982@gmail.com>
2023-04-11 04:18:05 -0700

    str to ptr

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -17,14 +17,14 @@ char *_strdup(char *str)
 	{
 		return (NULL);
 	}
-	ptr = malloc(sizeof(char) * strlen(str));
+	ptr = malloc(sizeof(char) * (strlen(str) + 1));
 	if (ptr == NULL || str == NULL)
 	{
 		return (NULL);
 	}
 	else
 	{
-		for (i = 0; str[i]; i++)
+		for (i = 0; i < strlen(str); i++)
 		{
 			ptr[i] = str[i];
 		}
