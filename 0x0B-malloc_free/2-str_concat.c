8717f199763f69450ef5e10f3e23efccc30c069d
Osama-Hesham <osamahesham982@gmail.com>
2023-04-11 05:04:28 -0700

    concatenate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,7 +13,13 @@ char *str_concat(char *s1, char *s2)
 	int i = 0;
 	int j = 0;
 	char *ptr =  malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
-
+	if(s1 == NULL && s2 == NULL)
+	{
+		s1 = "";
+		s2 = "";
+	}
+	else
+	{
 		if (s1 == NULL)
 		{
 			s1 = "";
@@ -22,6 +28,7 @@ char *str_concat(char *s1, char *s2)
 		{
 			s2 = "";
 		}
+	}
 	if (ptr == NULL)
 	{
 		return (NULL);
