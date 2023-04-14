e64d6c32d377ed13f4f30d7cdc29f854cf30ab14
Osama-Hesham <osamahesham982@gmail.com>
2023-04-13 19:56:06 -0700

    min max

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,7 +11,8 @@
 int *array_range(int min, int max)
 {
 	int i = 0;
-	int *ptr = malloc(sizeof(int) *  (max - min + 1));
+	int *ptr = malloc(sizeof(int) * (max - min + 1));
+
 	if (max > min || ptr == NULL)
 	{
 		return (NULL);
