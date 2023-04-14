7c30236a32f592abac4f3c5fadfd4423d9da4f4c
Osama-Hesham <osamahesham982@gmail.com>
2023-04-13 19:39:56 -0700

    concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * *string_nconcat - concat
+ * string_nconcat - function
  * @s1: input
  * @s2: input
  * @n: input
@@ -12,37 +12,36 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptr;
-	int i = 0;
-	int j = 0;
-
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	if (n < strlen(s2))
-		ptr = malloc(strlen(s1) + n + 1);
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+	
+	while (s1 != '\0')
+		len1++;
+	while (s2 != '\0')
+		len2++;
+	if (n < len2)
+		ptr = malloc(sizeof(char) * (len1 + n + 1));
 	else
-		ptr = malloc(strlen(s1) + strlen(s2) + 1);
+		ptr = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (ptr == NULL)
 		return (NULL);
-	for (i = 0; i < strlen(s1); i++)
+	while (i < len1)
+	{
 		ptr[i] = s1[i];
-	if (n < strlen(s2))
-	{
-		for (j = 0; j < n; j++)
-		{
-			ptr[i + j] = s2[j];
-			i++;
-		}
+		i++;
 	}
-	else
+	while (n < len2 && i < (len1 + n))
 	{
-		for (j = 0; j < strlen(s2); j++)
-		{
-			ptr[i + j] = s2[j];
-			i++;
-		}
+		ptr[i] = s2[j];
+		i++;
+		j++;
 	}
-	ptr[i + j] = '\0';
+	while (n >= len2 && i < (len1 + len2))
+	{
+		ptr[i] = s2[j];
+		i++;
+		j++;
+	}
+	ptr[i] = '\0';
 	return (ptr);
+
 }
