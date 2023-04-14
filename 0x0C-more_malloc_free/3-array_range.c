9c7b0cdc6962379694238855ca00f1c49f567911
Osama-Hesham <osamahesham982@gmail.com>
2023-04-13 19:54:25 -0700

    min max

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * *array_range - function
+ * @min: input
+ * @max: input
+ * Return: output
+ */
+int *array_range(int min, int max)
+{
+	int i = 0;
+	int *ptr = malloc(sizeof(int) *  (max - min + 1));
+	if (max > min || ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; min <= max; i++)
+	{
+		ptr[i] = min;
+		min++;
+	}
+	return (ptr);
+}
