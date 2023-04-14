51c28aca6b124fa4ee9386cbc178bb17941f31bd
Osama-Hesham <osamahesham982@gmail.com>
2023-04-13 19:30:08 -0700

    concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,29 +16,17 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	int j = 0;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 	if (n < strlen(s2))
-	{
 		ptr = malloc(strlen(s1) + n + 1);
-	}
 	else
-	{
 		ptr = malloc(strlen(s1) + strlen(s2) + 1);
-	}
 	if (ptr == NULL)
-	{
 		return (NULL);
-	}
 	for (i = 0; i < strlen(s1); i++)
-	{
 		ptr[i] = s1[i];
-	}
 	if (n < strlen(s2))
 	{
 		for (j = 0; j < n; j++)
