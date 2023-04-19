ec9ce1124c1d400ea61b06c3ea441ccc092a6a72
Osama-Hesham <osamahesham982@gmail.com>
2023-04-19 16:04:43 -0700

    op functions

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,28 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+/**
+ * get_op_function - executes an operation
+ * @s: input
+ * Return: output
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL},
+	};
+
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+	{
+		i++;
+	}
+
+	return (ops[i].f);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,53 @@
+#include "3-calc.h"
+
+/**
+ * op_add - add 2 numbers
+ * @a: input
+ * @b: input
+ * Return: output
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - sub 2 numbers
+ * @a: input
+ * @b: input
+ * Return: output
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiply 2 numbers
+ * @a: input
+ * @b: input
+ * Return: output
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divides 2 numbers
+ * @a: input
+ * @b: input
+ * Return: output
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - gets remainder od division of 2 numbers
+ * @a: input
+ * @b: input
+ * Return: output
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
