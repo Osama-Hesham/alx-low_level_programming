86dfd211004e2fbffbed6c706853bebcfd945103
Osama-Hesham <osamahesham982@gmail.com>
2023-04-19 16:12:08 -0700

    op functions

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,35 @@
+#include "function_pointers.h"
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+int main(int argc, char *argv[])
+{
+	char *op = argv[2];;
+	int a = atoi(argv[1]);
+	int b = atoi(argv[3]);
+
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((*op == '/' && b == 0) || (*op == '%' && b == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", get_op_func(op)(a, b));
+	return (0);
+}
