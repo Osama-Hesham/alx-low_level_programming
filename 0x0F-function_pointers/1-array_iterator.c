6391fb844d2c75519e327e50e25d21c897961d9f
Osama-Hesham <osamahesham982@gmail.com>
2023-04-19 15:29:39 -0700

    iterator array element

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ *  array_iterator - return elements
+ *  @array: input
+ *  @size: input
+ *  @action: input
+ *  Return: output
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i = 0;
+
+	if (action == NULL || array == NULL)
+	{
+		return;
+	}
+	
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
