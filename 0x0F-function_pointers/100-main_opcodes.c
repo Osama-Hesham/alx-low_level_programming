864080be39f5437c93cc0447f063557dc824b5b1
Osama-Hesham <osamahesham982@gmail.com>
2023-04-19 16:35:22 -0700

    opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,41 @@
+#include "function_pointers.h"
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+/**
+ * main - use op
+ * @argc: input
+ * @argv: input
+ * Return: output
+ */
+int main(int argc, char *argv[])
+{
+	int bytes = atoi(argv[1]);
+	int i = 0;
+	char *arr = (char *)main;;
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	
+	for (i = 0; i < bytes; i++)
+	{
+		if (i == bytes - 1)
+		{
+			printf("%x\n", arr[i]);
+			break;
+		}
+		printf("%x ", arr[i]);
+	}
+	return (0);
+
+}
