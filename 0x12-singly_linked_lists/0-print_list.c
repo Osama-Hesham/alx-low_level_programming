e398d6b54138f574de88c7230f9c274d6c04355e
Osama-Hesham <osamahesham982@gmail.com>
2023-04-27 12:21:49 -0700

    linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,20 +8,20 @@
  */
 size_t print_list(const list_t *h)
 {
-	 size_t list = 0;
+	size_t list = 0;
 
-	 while (h)
-	 {
-		  if (!h->str)
-		  {
-			   printf("[0] (nil)\n");
-		  }
-		  else
-		  {
-			  printf("[%u] %s\n", h->len, h->str);
-		  }
-		  h = h->next;
-		  list++;
-	 }
-	 return (list);
+	while (h)
+	{
+		if (!h->str)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		list++;
+	}
+	return (list);
 }
