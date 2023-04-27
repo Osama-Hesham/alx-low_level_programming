b11f3745ce46a8d61c0962c5676ac7f00a3396d3
Osama-Hesham <osamahesham982@gmail.com>
2023-04-27 12:50:03 -0700

    print before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+void print(void) __attribute__ ((constructor));
+/**
+ * print - print sentence
+ */
+void print(void)
+{
+	 printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
