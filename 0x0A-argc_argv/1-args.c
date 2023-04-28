7a38b4f7068dac98b4aaa3883109f2c45e01dda4
Osama-Hesham <osamahesham982@gmail.com>
2023-04-27 21:44:09 -0700

    number of args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+/**
+ * main - print number of args
+ * @argc: input
+ * @argv: input
+ * Return: output
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
+
