ee8e01c6e13acf9927094599012d196b4ca96d76
Osama-Hesham <osamahesham982@gmail.com>
2023-04-30 05:40:00 -0700

    print string

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,38 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.io>
+#include <stdarg.h>
+/**
+ * print_strings - print string
+ * @separator: input
+ * @n: input
+ * @...: input
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int  i = 0;
+	va_list list;
+	char *str;
+
+	va_start(list, n);
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(list, char *);
+
+		if (str == NULL)
+		{
+			printf("(nil)");
+		}
+		else
+		{
+			printf("%s", str);
+		}
+
+		if (index != n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(list);
+}
