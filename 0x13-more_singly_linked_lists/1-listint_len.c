2ad770c3adb132f911cf224fa528879671573b5d
Osama-Hesham <osamahesham982@gmail.com>
2023-04-30 08:43:44 -0700

    linked list length

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * listint_len - list length
+ * @h: input
+ * Return: output
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t i = 0;
+
+	 while (h)
+	 {
+		 i++;
+		 h = h->next;
+	 }
+	 return (i);
+}
