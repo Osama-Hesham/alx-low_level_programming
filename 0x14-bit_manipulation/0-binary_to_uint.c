372c85ed57f3f3a59307cf061229d4fcec87a888
Osama-Hesham <osamahesham982@gmail.com>
2023-05-04 09:22:57 -0700

    binary to decimal

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <string.h>
 /**
  * binary_to_uint - binary to decimal
@@ -9,7 +10,7 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0;
+	unsigned int n = 0;
 	unsigned int i = 0;
 
 	if (b != NULL)
@@ -26,7 +27,7 @@ unsigned int binary_to_uint(const char *b)
 			}
 			else
 			{
-				num = num * 2 + (b[i] - '0');
+				num += pow(2,i) * b[i] ;
 			}
 		}
 		return (num);
