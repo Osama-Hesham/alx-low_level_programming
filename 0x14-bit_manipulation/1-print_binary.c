c885cffded6ad3b05d8063727bac669837e7543c
Osama-Hesham <osamahesham982@gmail.com>
2023-05-04 11:51:49 -0700

    binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+/**
+ * print_binary - decimal to binary
+ * @n: input
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned int x = 0; 
+	unsigned int i = 0;
+	unsigned long int num;
+
+	for (i = 63; i >= 0; i--)
+	{
+		num = n >> i;
+
+		if (num & 1)
+		{
+			putchar('1');
+			x++;
+		}
+		else if (num)
+		{
+			putchar('0');
+		}
+	}
+	if (!count)
+	{
+		putchar('0');
+	}
+}
