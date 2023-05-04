88cd4ee67db5b53b794e1ca029f10173b6962064
Osama-Hesham <osamahesham982@gmail.com>
2023-05-04 09:40:33 -0700

    binary to decimal

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -21,8 +21,6 @@ unsigned int binary_to_uint(const char *b)
 	{
 		for (i = 0; b[i]; i++)
 		{
-			unsigned int j = 0;
-			unsigned int res = 0;
 
 			if (b[i] != '1' || b[i] != '0')
 			{
@@ -30,12 +28,7 @@ unsigned int binary_to_uint(const char *b)
 			}
 			else
 			{
-				for (j = 0; j <= i; i++)
-				{
-					res += j * 2;
-				}
-				
-				n += res * b[i] ;
+				n *= 2 + b[i] - '0';
 			}
 		}
 		return (n);
