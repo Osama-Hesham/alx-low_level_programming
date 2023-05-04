513b1bda5783fcd9f1f5174647817f9f9f0c4670
Osama-Hesham <osamahesham982@gmail.com>
2023-05-04 08:52:00 -0700

    binary to decima;

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * binary_to_uint - binary to decimal
+ * @b: input
+ * Return: output
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num = 0;
+	unsigned int i = 0;
+
+	if (b != NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		for (i = 0; b[i]; i++)
+		{
+			if (b[i] != '1' || b[i] != '0')
+			{
+				return (0);
+			}
+			else
+			{
+				num *= 2 + (b[i] - '0');
+			}
+		}
+		return (num);
+	}
+}
