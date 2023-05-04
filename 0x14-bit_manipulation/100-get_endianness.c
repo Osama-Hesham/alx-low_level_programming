d9be77f9e88c1d4c02547a3d72ff459c02dd53b3
Osama-Hesham <osamahesham982@gmail.com>
2023-05-04 12:20:01 -0700

    endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+/**
+ * get_endianness - check endian
+ * Return: output
+ */
+int get_endianness(void)
+{
+	char *c = c & i;
+
+	return (*c);
+}
