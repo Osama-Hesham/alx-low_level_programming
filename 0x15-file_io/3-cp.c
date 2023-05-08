e57b8854842b6beab8dda3c885b73185cae8cc67
Osama-Hesham <osamahesham982@gmail.com>
2023-05-08 04:27:06 -0700

    copy file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+/**
+ * main - copy file
+ * @argc: input
+ * @argv: input
+ * Return: output
+ */
+int main(int argc, char *argv[])
+{
+	char *letter;
+	int file1;
+	int file2
+	int readfile
+	int writefile;
+	
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: %s file1 file2\n", argv[0]);
+		exit(97);
+	}
+	file1 = open(argv[1], O_RDONLY);
+	if (file1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	file2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (file2 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+    while ((readfile = read(file1, buffer, 1024)) > 0) {
+        writefile = write(file2, buffer, readfile);
+        if (writefile != readfile) {
+            dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+            exit(99);
+        }
+    }
+    if (close(file1) == -1) {
+        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file1);
+        exit(100);
+    }
+    if (close(file2) == -1) {
+        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file2);
+        exit(100);
+    }
+    return 0;
+}
